2eaf5f00fedefd2fac3d77bbb709055038c5498c
dashMrl <614626892@qq.com>
2018-05-05 15:41:34 +0800

    config: add push action to deploy script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -23,4 +23,5 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "`date`"
+cd public && git add --all && git commit -m "`date`" && cd ..
+git push origin gh-pages
